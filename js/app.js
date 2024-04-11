559c093a2aa704a481f2a21713cb880e7bb55ced
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2024-04-11 11:51:05 -0300

    Alterando a aplicação para uma PWA.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+const musics = document.querySelectorAll(".playlist li");
+console.log(musics);
+
+const buttons = document.querySelectorAll(".cifra a");
+for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    const buttonParent = e.target.closest(".cifra").closest("li");
+    const musicLyric = buttonParent.querySelector(".music-lyric");
+    musicLyric.style.display =
+      musicLyric.style.display === "block" ? "none" : "block";
+  });
+}
+
+const searchInput = document.querySelector("#searchInput");
+searchInput.addEventListener("input", (e) => {
+  const inputText = e.target.value;
+  for (let music of musics) {
+    const musicName = music.querySelector("h4").innerText;
+    if (musicName.toLowerCase().includes(inputText.toLowerCase())) {
+      music.style.display = "block";
+    } else {
+      music.style.display = "none";
+    }
+  }
+});
+
 // app.js
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
