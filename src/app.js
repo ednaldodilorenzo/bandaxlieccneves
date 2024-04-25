ac1105d73e38aeff36f3244643b2ad08e8330bdc
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2024-04-25 14:44:50 -0300

    Alterações para audio player e estilo de música sendo tocada.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 // Import the functions you need from the SDKs you need
 import "./css/styles.css";
-import { PlayList } from "./js/views";
+import { PlayList, AudioPlayer } from "./js/views";
 
 let musicList = [];
 
 document.addEventListener("DOMContentLoaded", async () => {  
-  const requestModule = await import(import.meta.env.DEV ? "./js/mock-request.js" : "./js/request.js")
+  const requestModule = await import(import.meta.env.DEV ? "./js/request.js" : "./js/request.js")
   musicList = await requestModule.fetchCollectionData();
   
   const playList = new PlayList(musicList);
+  const audioPlayer = new AudioPlayer("audio-player");
 
   playList.render("app", musicList);
+  playList.addEventListener("playlist-music-played", (e) => {
+    audioPlayer.play(e.data);
+  });
+
+  audioPlayer.addEventListener("previous-clicked", () => {
+    audioPlayer.play(playList.getPreviousMusic());
+  });
+
+  audioPlayer.addEventListener("next-clicked", () => {
+    audioPlayer.play(playList.getNextMusic());
+  });
 
   const searchInput = document.querySelector("#searchInput");
   searchInput.addEventListener("input", (e) => {
@@ -21,35 +33,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     searchInput.value = "";
     searchInput.focus();
     playList.filterMusics("");
-  });
-
-  document.addEventListener(
-    "play",
-    function (e) {
-      const playedId = e.target.closest("li");
-      playList.playId(playedId.id);
-    },
-    true
-  );
-
-  document.addEventListener(
-    "ended",
-    function (e) {
-      playList.playNext();
-    },
-    true
-  );
-
-  const audioPlayerPlayButton = document.getElementById(
-    "audioPlayerPlayButton"
-  );
-  const audioPlayer = document.getElementById("audioPlayer");
-  audioPlayerPlayButton.addEventListener("click", async () => {
-    const fileURL = await getFileURL("audios/acaso-nao-sabeis.mp3");
-    audioPlayer.src = fileURL;
-    audioPlayer.load();
-    audioPlayer.play();
-  });
+  });  
 });
 
 // app.js
--- src/js/views.js
+++ src/js/views.js
@@ -10,38 +10,83 @@ const changeTextSize = (e) => {
     : currentSize - 1 + "px";
 };
 
-export class Music {
+class EventEmitter {
+  #callbacks = {};
+
+  static Event = class {
+    type = "";
+    target = "";
+    data = "";
+  };
+
+  addEventListener(eventType, callback) {
+    if (typeof callback !== "function") return;
+    if (this.#callbacks[eventType] === undefined) {
+      this.#callbacks[eventType] = [];
+    }
+
+    this.#callbacks[eventType].push(callback);
+  }
+
+  dispatchEvent(eventType, data) {
+    if (this.#callbacks[eventType] === undefined) return;
+
+    const event = new EventEmitter.Event();
+    event.type = eventType;
+    event.target = this;
+    event.data = data;
+
+    this.#callbacks[eventType].forEach((callback) => {
+      callback(event);
+    });
+  }
+}
+
+export class Music extends EventEmitter {
   constructor(title, source, theme, tone, cipher, id) {
+    super();
     this.title = title;
     this.source = source;
     this.theme = theme;
     this.tone = tone;
     this.cipher = cipher;
     this.id = id;
+    this.rootElement = document.createElement("li");
+  }
+
+  setPlaying() {
+    this.rootElement.style.backgroundColor = "#ddb0b0";
+  }
+
+  setNotPlaying() {
+    this.rootElement.style.backgroundColor = "white";
   }
 
   render() {
-    const rootElement = document.createElement("li");
-    rootElement.id = this.id;    
-    rootElement.innerHTML = `
+    this.rootElement.id = this.id;
+    this.rootElement.innerHTML = `
             <h3 class="music-list_item__title">${this.title}</h3>
             <div class="music-list_item__details">
               <h5 class="music-list_item__details__tone">Tom: ${this.tone}</h5>
               <h5 class="music-list_item__details__theme">${this.theme}</h5>
             </div>
-            <audio class="music-list_item__audio" preload="metadata" controls>
-              <source src="${this.source}" type="audio/mp3" />
-              Seu navegador não suporta o elemento de áudio.
-            </audio>
+            <button class="musicPlayButton">Play</button>
             <div class="music-list_item__cipher">
               <a class="music-list_item__cipher__link" href="cifras/${this.cipher}".html>Ver Cifra</a>
             </div>
         `;
 
-    const viewCipherButton = rootElement.querySelector(".music-list_item__cipher__link");
+    const musicPlayButton = this.rootElement.getElementsByTagName("button")[0];
+    musicPlayButton.addEventListener("click", () => {
+      this.dispatchEvent("music-played", this);
+    });
+
+    const viewCipherButton = this.rootElement.querySelector(
+      ".music-list_item__cipher__link"
+    );
     viewCipherButton.addEventListener("click", (e) => {
       e.preventDefault();
-      let cipherElement = rootElement.querySelector(".modal");
+      let cipherElement = this.rootElement.querySelector(".modal");
       if (cipherElement) {
         cipherElement.style.display =
           cipherElement.style.display === "block" ? "none" : "block";
@@ -84,27 +129,35 @@ export class Music {
           cipherContent.innerHTML = html;
           modalContent.appendChild(cipherContent);
           cipherElement.style.display = "block";
-          rootElement.appendChild(cipherElement);
+          this.rootElement.appendChild(cipherElement);
         })
         .catch((error) => {
           console.error("There was a problem with the fetch operation:", error);
         });
     });
 
-    return rootElement;
+    return this.rootElement;
   }
 }
 
-export class PlayList {
+export class PlayList extends EventEmitter {
   constructor() {
+    super();
+    this.musics = [];
+    this.currentIndex = -1;
     this.currentPlaying = "";
     this.playlistElement = document.createElement("ul");
     this.playlistElement.className = "playlist";
   }
 
   render(elementId, musics) {
+    this.musics = musics;
+    this.currentIndex = musics ? 0 : -1;
     const rootElement = document.getElementById(elementId);
     for (const music of musics) {
+      music.addEventListener("music-played", (e) => {
+        this.dispatchEvent("playlist-music-played", e.target);
+      });
       this.playlistElement.appendChild(music.render());
     }
 
@@ -135,6 +188,24 @@ export class PlayList {
     this.currentPlaying = id;
   }
 
+  getNextMusic() {
+    this.musics[this.currentIndex].setNotPlaying();
+    this.currentIndex =
+      this.currentIndex + 1 > this.musics.length ? 0 : (this.currentIndex += 1);
+    this.musics[this.currentIndex].setPlaying();
+    return this.musics[this.currentIndex];
+  }
+
+  getPreviousMusic() {
+    this.musics[this.currentIndex].setNotPlaying();
+    this.currentIndex =
+      this.currentIndex === 0
+        ? this.musics.length - 1
+        : (this.currentIndex -= 1);
+    this.musics[this.currentIndex].setPlaying();
+    return this.musics[this.currentIndex];
+  }
+
   playNext() {
     const music = document.getElementById(this.currentPlaying);
     const musicAudio = music.querySelector("audio");
@@ -157,5 +228,102 @@ export class PlayList {
         }
       }
     }
+  }
+}
+
+export class AudioPlayer extends EventEmitter {
+  constructor(rootElementId) {
+    super();
+    this.playing = false;
+    this.paused = false;
+    this.currentMusic = undefined;
+    this.rootElement = document.getElementById(rootElementId);
+    const playPauseButton = this.rootElement.querySelector(
+      "#audioPlayerPlayPauseButton"
+    );
+    playPauseButton.addEventListener("click", () => {
+      if (this.playing) {
+        this.pause();
+      } else {
+        this.play(this.currentMusic);
+      }
+    });
+    const closeButton = this.rootElement.querySelector(
+      "#audioPlayerCloseButton"
+    );
+    closeButton.addEventListener("click", () => {
+      this.pause();
+      this.rootElement.style.display = "none";
+    });
+    const previousButton = this.rootElement.querySelector(
+      "#audioPlayerPreviousButton"
+    );
+    previousButton.addEventListener("click", () => {
+      this.dispatchEvent("previous-clicked");
+    });
+
+    const nextButton = this.rootElement.querySelector("#audioPlayerNextButton");
+    nextButton.addEventListener("click", () => {
+      this.dispatchEvent("next-clicked");
+    });
+
+    const progressBar = document.getElementById("progress-bar");
+    const progress = document.getElementById("progress");
+    const progressHandle = document.getElementById("progress-handle");
+    const audio = this.rootElement.querySelector("audio");
+    audio.addEventListener("timeupdate", () => {
+      const percentage = (audio.currentTime / audio.duration) * 100;
+      progress.style.width = percentage + "%";
+      progressHandle.style.left = `calc(${percentage}% - 5px)`;
+    });
+
+    audio.addEventListener("ended", () => {
+      this.dispatchEvent("next-clicked");
+    });
+
+    progressBar.addEventListener("click", (e) => {
+      const barWidth = progressBar.clientWidth;
+      const clickPosition = e.offsetX;
+      const time = (clickPosition / barWidth) * audio.duration;
+      audio.currentTime = time;
+    });
+  }
+
+  play(music) {
+    if (this.currentMusic) {
+      this.currentMusic.setNotPlaying();
+    }
+    this.currentMusic = music;
+    this.currentMusic.setPlaying();
+    this.playing = true;
+    const audio = this.rootElement.querySelector("audio");
+    if (this.paused) {
+      audio.play();
+      return;
+    }
+    this.paused = false;
+    this.rootElement.style.display = "block";
+    audio.src = music.source;
+    const playPauseButton = this.rootElement.querySelector(
+      "#audioPlayerPlayPauseButton"
+    );
+    playPauseButton.getElementsByTagName("img")[0].src =
+      "images/pause-regular-24.png";
+    const playingMusicTitle = this.rootElement.querySelector(
+      "#playing-music__title"
+    );
+    playingMusicTitle.innerText = music.title;
+    audio.play();
+  }
+
+  pause() {
+    const audio = this.rootElement.querySelector("audio");
+    this.playing = false;
+    audio.pause();
+    const playPauseButton = this.rootElement.querySelector(
+      "#audioPlayerPlayPauseButton"
+    );
+    playPauseButton.getElementsByTagName("img")[0].src =
+      "images/play-regular-24.png";
   }
 }
