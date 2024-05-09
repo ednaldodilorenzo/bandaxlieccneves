cda1ebc7135645babfd5a1f7052c639b480296e6
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2024-05-09 10:17:43 -0300

    Melhorando o gerenciamento do cache.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,26 +1,44 @@
+const CURRENT_CACHE_STATIC = "static-v3";
+
 // service-worker.js
-self.addEventListener('install', event => {
-    console.log('Service worker installing...');
-    // Pre-cache a route
-    event.waitUntil(
-        caches.open('static-v2').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/js/app.js',
-                '/js/request.js',
-                '/js/vendor.js',
-            ]);
-        })
-    );
+self.addEventListener("install", (event) => {
+  console.log("Service worker installing...");
+  // Pre-cache a route
+  event.waitUntil(
+    caches.open(CURRENT_CACHE_STATIC).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/js/app.js",
+        "/js/request.js",
+        "/js/vendor.js",
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url);
-    // Cache-First Strategy
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CURRENT_CACHE_STATIC) {
+            console.log("[Service worker] Removing old cache", key);
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
+  // Cache-First Strategy
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
