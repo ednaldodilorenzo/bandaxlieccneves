64d844a66f2ab0ce9c02d2dea12c1a435eb42ba0
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2024-05-09 22:49:56 -0300

    Alterando o service worker.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
 const CURRENT_CACHE_STATIC = "static-v3";
-const CURRENT_CACHE_AUDIO = "audio-v1";
 
 // service-worker.js
 self.addEventListener("install", (event) => {
@@ -23,7 +22,7 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
-          if (key !== CURRENT_CACHE_STATIC && key !== CURRENT_CACHE_AUDIO) {
+          if (key !== CURRENT_CACHE_STATIC) {
             console.log("[Service worker] Removing old cache", key);
             return caches.delete(key);
           }
@@ -36,41 +35,10 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   console.log("Fetching:", event.request.url);
+  // Cache-First Strategy
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response.
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then((fetchResponse) => {
-        if (
-          !fetchResponse ||
-          fetchResponse.status >= 300 ||
-          fetchResponse.type !== "basic"
-        ) {
-          return fetchResponse;
-        }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        if (event.request.url.includes("firebasestorage.googleapis.com")) {
-          var responseToCache = fetchResponse.clone();
-
-          caches.open(CURRENT_CACHE_AUDIO).then((cache) => {
-            cache.put(event.request, responseToCache);
-          });
-        }
-
-        return fetchResponse;
-      });
+      return response || fetch(event.request);
     })
   );
 });
